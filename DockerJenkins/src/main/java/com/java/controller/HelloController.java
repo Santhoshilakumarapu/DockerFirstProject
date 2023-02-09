7a5327b72cb52dd7bf294f2eb162edb822697e86
Santhoshilakumarapu <santhoshilakumarapu@gmail.com>
2023-02-09 13:08:15 -0500

    first commit

---
+++ DockerJenkins/src/main/java/com/java/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.java.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/messages")
+	public String welcome()
+	{
+		return "welcome to Docker using Jenkins";
+	}
+
+}
